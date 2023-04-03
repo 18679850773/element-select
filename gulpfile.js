b8672f715f6b3aa4064dfec161908fc5020d096f
o_chenxiaowen <18679850773@163.com>
2023-04-03 10:25:14 +0800

    updata

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'), connect = require('gulp-connect')
+// var stripDebug = require('gulp-strip-debug');
+const uglify = require('gulp-uglify')
+// const rename = require('gulp-rename')
+
+var path = {
+  dev: './src/',
+  build: "./dist/"
+}
+
+gulp.task('serve', function() {
+  connect.server({
+    name: '框选页面上任意dom元素',
+    port: 8888,
+    // root: path.dev,
+    // livereload: true
+  })
+})
+
+gulp.task('build', async function() {
+  gulp.src(path.dev+'*.js')
+    // .pipe(stripDebug())
+    .pipe(uglify())
+    // .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(path.build))
+})
